724f7ba26ff461cbdb411984da771fccda8e514a
ccuqme <mw@emqu.cc>
2023-06-13 00:13:37 +0200

    fix: set-default graphical.target attempt 4

--- etc/ublue-sddm-workaround.sh
+++ etc/ublue-sddm-workaround.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+if [ -f /var/lib/sddm/.ublue-workaround ]; then
+    exit 0
+fi
 
 if [ ! -d /var/lib/sddm ]; then mkdir /var/lib/sddm; fi
 if [ ! -d /var/log/sddm ]; then mkdir /var/log/sddm; fi
@@ -7,3 +9,7 @@ getent group sddm >/dev/null || groupadd -r sddm
 getent passwd sddm >/dev/null || useradd -g sddm -M -d /var/lib/sddm -s /sbin/nologin -r sddm
 
 systemctl set-default graphical.target
+
+touch /var/lib/sddm/.ublue-workaround
+
+shutdown -r +0 "Rebooting into SDDM"
