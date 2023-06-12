c95b75607a4f84bb12e7fc3544a97f878e8de2c6
ccuqme <mw@emqu.cc>
2023-06-13 00:49:21 +0200

    fix: set-default graphical.target last attempt for today

--- etc/profile.d/hyprland-firstboot.sh
+++ etc/profile.d/hyprland-firstboot.sh
@@ -2,5 +2,11 @@ if test "$(id -u)" -gt "0" && test -d "$HOME"; then
     if test ! -e "$HOME"/.hyprland-configured; then
         touch "$HOME"/.hyprland-configured
         cp -R /usr/share/homedir/.* "$HOME"/
+        if [ -f /var/lib/sddm/state.conf ]; then
+            exit 0
+        fi
+        echo "Rebooting into SDDM in 5 seconds..." | wall
+        sleep 5
+        systemctl reboot
     fi
 fi
