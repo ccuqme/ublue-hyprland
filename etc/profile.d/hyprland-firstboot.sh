2544aaf9a961533d8be00c966972445a25d618c6
ccuqme <mw@emqu.cc>
2023-06-13 01:09:55 +0200

    fix: Reboot on first login

--- etc/profile.d/hyprland-firstboot.sh
+++ etc/profile.d/hyprland-firstboot.sh
@@ -2,11 +2,10 @@ if test "$(id -u)" -gt "0" && test -d "$HOME"; then
     if test ! -e "$HOME"/.hyprland-configured; then
         touch "$HOME"/.hyprland-configured
         cp -R /usr/share/homedir/.* "$HOME"/
-        if [ -f /var/lib/sddm/state.conf ]; then
-            exit 0
+        if [ ! -f /var/lib/sddm/state.conf ]; then
+            echo "Rebooting into SDDM in 5 seconds..." | wall
+            sleep 5
+            systemctl reboot
         fi
-        echo "Rebooting into SDDM in 5 seconds..." | wall
-        sleep 5
-        systemctl reboot
     fi
 fi
