79b1767401b865864488e1f0d62c48318533d817
ccuqme <mw@emqu.cc>
2023-06-14 01:41:07 +0200

    fix: correctly checking if sddm is already running

--- etc/profile.d/hyprland-firstboot.sh
+++ etc/profile.d/hyprland-firstboot.sh
@@ -2,7 +2,7 @@ if test "$(id -u)" -gt "0" && test -d "$HOME"; then
     if test ! -e "$HOME"/.hyprland-configured; then
         touch "$HOME"/.hyprland-configured
         cp -R /usr/share/homedir/.* "$HOME"/
-        if [ ! -f /var/lib/sddm/state.conf ]; then
+        if ! systemctl --quiet is-active sddm; then
             echo "Rebooting into SDDM in 5 seconds..." | wall
             sleep 5
             systemctl reboot
